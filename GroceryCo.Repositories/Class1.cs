d687349507bed0f553e54140dbd7ec07f1261b40
jsauter <jsauter@G20500>
2017-01-31 09:15:06 -0700

    first commit with projects for services and repos.

---
+++ GroceryCo.Repositories/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryCo.Repositories
+{
+    public class Class1
+    {
+    }
+}
